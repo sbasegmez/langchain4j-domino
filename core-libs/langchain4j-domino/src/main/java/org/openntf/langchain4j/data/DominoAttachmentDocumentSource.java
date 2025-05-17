5c5f876fa279513f45c8efbfffee30e1767f2522
Serdar <sbasegmez@users.noreply.github.com>
2025-05-17 09:47:02 +0100

    License headers updated

--- core-libs/langchain4j-domino/src/main/java/org/openntf/langchain4j/data/DominoAttachmentDocumentSource.java
+++ core-libs/langchain4j-domino/src/main/java/org/openntf/langchain4j/data/DominoAttachmentDocumentSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) ${project.inceptionYear}-2025 Serdar Basegmez
+ * Copyright (c) 2024-2025 Serdar Basegmez
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
